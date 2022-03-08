cf901d3112314752a10ba6ced6d4be8d16db044a
최지은 <zzieunchoi0707@gmail.com>
2022-03-08 18:35:50 +0900

    220308

--- TIL/4. Django/crud/articles/views.py
+++ TIL/4. Django/crud/articles/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html',context)
+
+
+def new(request):
+
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    # 첫번째 방법
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    #두번쨰 방법
+    article = Article(title = title, content = content)
+    article.save()
+    
+    #세번째 방법
+    # Article.objects.create(title= title, content= content)
+    return render(request, 'articles/create.html')
